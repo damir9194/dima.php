85b2710f073574b89d17014b71c2f38b8a3a89ce
damir9194 <46625390+damir9194@users.noreply.github.com>
2019-01-12 13:27:41 +0000

    Update and rename README.md to dima.php

--- README.md
+++
@@ -1,2 +0,0 @@
-# dima.php
-dima.php
---
+++ dima.php
@@ -0,0 +1,138 @@
+
+
+<?php
+ob_start();
+define('API_KEY','642082179:AAHRrXNffMGlOP-hnTE3xBRUGZ5-rPphH8w');
+//tokenni yozing
+$admin = "85906885"; 
+//ozizni id raqamizni yozing
+
+//ozizni id raqamizni yozing
+
+   function del($nomi){
+   array_map('unlink', glob("$nomi"));
+   }
+
+   function ty($ch){ 
+   return bot('sendChatAction', [
+   'chat_id' => $ch,
+   'action' => 'typing',
+   ]);
+} 
+
+function bot($method,$datas=[]){
+    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
+$update = json_decode(file_get_contents('php://input'));
+$edname = $editm ->from->first_name;
+$message = $update->message;
+$mesid = $message->message_id;
+$mid = $message->message_id;
+$chat_id = $message->chat->id;
+$cid = $message->chat->id;
+$mtext = $message->text;
+$capt = $message->caption;
+$forward = $update->message->forward_from;
+$editm = $update->edited_message;
+$fadmin = $message->from->id;
+$cty = $message->chat->type;
+$step=file_get_contents("data/$fadmin/name.txt");
+//bu yerni o'zgartirishingiz mumkin.
+
+$update = json_decode(file_get_contents('php://input'));
+$data = $update->callback_query->data;
+$cid2 = $update->callback_query->message->chat->id; 
+$cqid = $update->callback_query->id;
+$chat_id2 = $update->callback_query->message->chat->id;
+$message_id2 = $update->callback_query->message->message_id;
+$botim="dima9194bot";
+$soat = date('H:i', strtotime('5 hour'));
+$message = $update->message;
+$mid = $message->message_id;
+$chat_id = $message->chat->id;
+$text1 = $message->text;
+$gif = $message->animation;
+$doc = $message->document;
+$title = $message->chat->title;
+$fadmin = $message->from->id;
+$lang = $message->from->language_code;
+$from = $message->from;
+$id = $message->reply_to_message->from->id;
+$message_id = $message->reply_to_message->message_id;
+$from_user_first_name = $message->reply_to_message->from->first_name;
+//Yangi odam id si
+$new_chat_members = $message->new_chat_member->id;
+$lan = $message->new_chat_member->language_code;
+$ism = $message->new_chat_member->first_name;
+$username = $message->from->username;
+$first_name = $message->from->first_name;
+$is_bot = $message->new_chat_member->is_bot;
+$step = file_get_contents("stat/$chat_id.step");
+$guruhlar = file_get_contents("stat/group.list");
+$userlar = file_get_contents("stat/user.list");
+mkdir("warn");
+mkdir("stat");
+
+
+    if (($new_chat_members != NUll)&&($is_bot!=true)) {
+  if((stripos($lan,"fa")!== false) or (stripos($lan,"ar")!==false)){
+      $vaqti = strtotime("+999999999999 minutes");
+  bot('kickChatMember', [
+      'chat_id' => $chat_id,
+      'user_id' => $new_chat_members,
+      'until_date'=> $vaqti,
+    ]);
+    }else{
+      $test = "<b>👋Salom</b> <a href='tg://user?id=$new_chat_members'>$ism</a> ,<b>$title</b> guruhimizga xush kelibsiz!";
+       bot('sendmessage',[
+       'chat_id'=>$chat_id,
+       'text'=>$test,
+       'parse_mode'=>'html'
+     ]);
+   }
+    }
+
+if(isset($text1)){
+  if($cty == "group" or $cty == "supergroup"){
+    if(stripos($guruhlar,"$chat_id")!==false){
+    }else{
+    file_put_contents("stat/group.list","$guruhlar\n$chat_id");
+    }
+  }else{
+   $userlar = file_get_contents("stat/user.list");
+   if(stripos($userlar,"$chat_id")!==false){
+    }else{
+    file_put_contents("stat/user.list","$userlar\n$chat_id");
+   }
+  }
+ }
+  
+ 
+
+
+    if (($new_chat_members != NUll)&&($is_bot!=false)) {
+$gett = bot('getChatMember', [
+'chat_id' => $chat_id,
+'user_id' => $fadmin,
+]);
+$get = $gett->result->status;
+if($get =="member"){
+   $vaqti = strtotime("+999999999999 minutes");
+  bot('kickChatMember', [
+      'chat_id' => $chat_id,
+      'user_id' => $new_chat_members,
+      'until_date'=> $vaqti,
+  ]);
+  bot('sendChatAction',['chat_id'=>$chat_id,'action'=>"typing"]);
+  bot('sendmessage', [
+      'cha
